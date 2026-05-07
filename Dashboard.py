85fa2cf75e5323a55a286326244eb0848f291c89
Amos-Kasyoka <akkasyoka@gmail.com>
2026-05-07 14:07:46 +0300

    Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -1,9 +1,10 @@
 import streamlit as st
 import pandas as pd
+import plotly.express as px
 
-st.set_page_config(page_title="Sales Dashboard", page_icon="📊")
+st.set_page_config(page_title="Sales Dashboard", page_icon="📊", layout="wide")
 
-st.title("📊 Sales Dashboard")
+st.title("📊 Interactive Sales Dashboard")
 st.sidebar.header("🔍 Filter Data")
 
 df = pd.read_csv("Online_Sales_Data.csv")
@@ -24,10 +25,12 @@ filtered_df = df[
     (df["Region"].isin(region_filter)) &
     (df["Product Category"].isin(product_filter))
 ]
+
 st.markdown(
-    f"#### Region Selected: {region_filter}\n"
-    f"#### Product Selected: {product_filter}"
+    f"#### 🌍 Region Selected: {region_filter}\n"
+    f"#### 🛒 Product Selected: {product_filter}"
 )
+
 col1, col2 = st.columns(2)
 
 with col1:
@@ -35,3 +38,43 @@ with col1:
 
 with col2:
     st.metric("📈 Average Revenue", f"${filtered_df['Total Revenue'].mean():,.2f}")
+
+tab1, tab2 = st.tabs(["🛒 Sales by Product", "🌍 Sales by Region"])
+
+with tab1:
+    st.subheader("Sales by Product")
+
+    product_data = (
+        filtered_df.groupby("Product Category")["Total Revenue"]
+        .sum()
+        .reset_index()
+    )
+
+    fig1 = px.bar(
+        product_data,
+        x="Product Category",
+        y="Total Revenue",
+        color="Product Category",
+        text="Total Revenue",
+        title="Sales by Product"
+    )
+
+    st.plotly_chart(fig1, use_container_width=True)
+
+with tab2:
+    st.subheader("Sales by Region")
+
+    region_data = (
+        filtered_df.groupby("Region")["Total Revenue"]
+        .sum()
+        .reset_index()
+    )
+
+    fig2 = px.pie(
+        region_data,
+        names="Region",
+        values="Total Revenue",
+        title="Sales by Region"
+    )
+
+    st.plotly_chart(fig2, use_container_width=True)
